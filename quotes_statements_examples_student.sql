47a8ac921007103a1daace5adec3e89eae154e32
Greg Jett <greg.jett25@gmail.com>
2020-07-07 11:22:56 -0500

    files

--- quotes_statements_examples_student.sql
+++ quotes_statements_examples_student.sql
@@ -26,10 +26,15 @@ SELECT quote
 FROM quotes
 WHERE author_last_name = 'Adams';
 -- TODO TOGETHER: View quotes by Douglas Adams. Add a caption
+SELECT quote
+AS 'Douglas Adams Quotes'
+FROM quotes
+WHERE author_last_name = 'Adams';
 
 -- TODO TOGETHER: View quote with primary key (in this case id) of 5
 
 -- TODO TOGETHER: Select all from quotes where quote contains(is like) "the"
+SELECT * FROM quotes WHERE quote LIKE '%the%';
 
 
 
@@ -37,16 +42,26 @@ WHERE author_last_name = 'Adams';
 
 /*********************** UPDATE ***********************/
 -- TODO TOGETHER: View all quotes
+SELECT * from quotes;
 
 
 -- TODO TOGETHER: Update quote 4 to first name = Samuel and last name = Clemens
+SELECT * FROM quotes WHERE id = 4;
+SELECT author_first_name, author_last_name FROM quotes WHERE id = 4;
+
+UPDATE quotes SET author_first_name = 'Samuel', author_last_name = 'Clemens' WHERE id = 4;
+
+
 
 /*********************** DELETE ***********************/
 
 -- TODO TOGETHER: Delete quote with id 3. Then view the output.
+SELECT * FROM quotes WHERE id = 3;
+
+DELETE FROM quotes WHERE id = 3;
 
 
 
 /*********************** TRUNCATE ***********************/
 -- TODO TOGETHER: Truncate quotes table.... there is no going back! Deletes all records.
-
+TRUNCATE quotes;
