b61c6fc8c270e2853c3138c0d6a247b955645130
Greg Jett <greg.jett25@gmail.com>
2020-07-09 13:34:36 -0500

    files

---
+++ aliases_exercises.sql
@@ -0,0 +1,7 @@
+use employees;
+
+SELECT CONCAT(emp_no, ' - ' ,last_name, '',first_name)
+    AS full_name, birth_date AS DOB
+FROM employees
+GROUP BY emp_no, last_name, first_name, birth_date
+limit 10;
--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,1 +1,9 @@
 use employees;
+
+SELECT DISTINCT titles FROM titles;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE %e%
+GROUP BY first_name, last_name
+
